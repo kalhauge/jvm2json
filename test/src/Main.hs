b43c77c6673c6c51d5c24e1cc6f3175b0c5acb4e
Christian Gram Kalhauge <chrg@dtu.dk>
2023-09-11 10:09:51 +0200

    Add golden test for usage information

--- test/src/Main.hs
+++ test/src/Main.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Test.Tasty
 import Test.Tasty.Golden as G
 
 import qualified Data.Text.Lazy.Encoding as Text
+
+import System.Process.Typed
 
 import Prettyprinter
 import Prettyprinter.Render.Text
@@ -16,6 +20,7 @@ tests =
   testGroup
     "Tests"
     [ codec
+    , usage
     ]
 
 codec :: TestTree
@@ -23,4 +28,12 @@ codec =
   goldenVsString
     "codec"
     "CODEC.txt"
-    (pure . Text.encodeUtf8 . renderLazy $ layoutPretty defaultLayoutOptions prettyClass)
+    (pure . Text.encodeUtf8 . renderLazy $ layoutPretty defaultLayoutOptions (prettyClass <> line))
+
+usage :: TestTree
+usage =
+  goldenVsString
+    "codec"
+    "USAGE.txt"
+    ( readProcessStdout_ "cabal run jvm2json -- --help"
+    )
